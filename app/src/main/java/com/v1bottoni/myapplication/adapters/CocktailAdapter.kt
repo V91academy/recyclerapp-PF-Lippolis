e022bf42f8c48f10a8d03810c796f33ac515f4cf
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-09 17:42:43 +0200

    Add image loading into CocktailAdapter

--- app/src/main/java/com/v1bottoni/myapplication/adapters/CocktailAdapter.kt
+++ app/src/main/java/com/v1bottoni/myapplication/adapters/CocktailAdapter.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.v1bottoni.myapplication.R
 import com.v1bottoni.myapplication.databinding.BigCardviewBinding
 import com.v1bottoni.myapplication.databinding.SmallCardviewBinding
@@ -75,6 +78,13 @@ class CocktailAdapter(val list: List<Cocktail>): RecyclerView.Adapter<CocktailAd
         holder.recipe.text = cocktail.recipe
         holder.ingredients.adapter = IngredientAdapter(cocktail.ingredients)
 
+        val circularProgressDrawable = CircularProgressDrawable(this)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+        Glide.with(holder.image.context)
+            .load(cocktail.image)
+            .placeholder(circularProgressDrawable).into(holder.image)
     }
 
     override fun getItemCount(): Int {
