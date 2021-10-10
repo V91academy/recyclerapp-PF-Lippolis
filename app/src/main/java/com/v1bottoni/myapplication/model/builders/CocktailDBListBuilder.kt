82e74c9d77194506399bf1eeac0d4249fdf9ab7f
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-10 14:06:36 +0200

    Add read json string functionality to CocktailDBListBuilder

--- app/src/main/java/com/v1bottoni/myapplication/model/builders/CocktailDBListBuilder.kt
+++ app/src/main/java/com/v1bottoni/myapplication/model/builders/CocktailDBListBuilder.kt
@@ -1,17 +1,22 @@
 package com.v1bottoni.myapplication.model.builders
 
+import android.os.Build
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.annotation.RequiresApi
+import com.v1bottoni.myapplication.CocktailListFragment
 import com.v1bottoni.myapplication.model.Cocktail
+import com.v1bottoni.myapplication.model.Ingredient
+import org.json.JSONObject
 import java.lang.IllegalStateException
+import java.nio.file.Paths
 
 /**
  * Class responsible for creating a List<Cocktail> using data from the site www.thecocktaildb.com
  */
 class CocktailDBListBuilder(
     val searchType: SearchType? = null,
-    val searchString: String? = null,
-    val resourceID: Int? = null
+    val searchString: String? = null
 ) : CocktailListBuilder {
 
     enum class SearchType(val url: String): Parcelable {
@@ -39,15 +44,37 @@ class CocktailDBListBuilder(
 
     override fun build(): List<Cocktail> {
         return when(searchType) {
-            SearchType.LOCAL_JSON -> listOf()
+            SearchType.LOCAL_JSON -> searchString?.let {buildFromJSONString(it)} ?: throw IllegalStateException()
             null -> throw IllegalStateException("CocktailDBListBuilder needs a searchType before it can build")
         }
     }
 
+    private fun buildFromJSONString(jsonString: String): List<Cocktail> {
+        val cocktails = JSONObject(jsonString).getJSONArray("drinks")
+        var cocktailsList = mutableListOf<Cocktail>()
+        for(i in 0 until cocktails.length()){
+            val cocktail = cocktails.getJSONObject(i)
+            val name = cocktail.getString("strDrink") ?: ""
+            val recipe = cocktail.getString("strInstructions") ?:""
+            val ingredients = mutableListOf<Ingredient>()
+            var j = 1
+            var ingredientName: String? = null
+            ingredientName = cocktail.getString("strIngredient$j")
+            while(!ingredientName.isNullOrEmpty()) {
+                val ingredientQuantity = cocktail.getString("strMeasure$j") ?: ""
+                ingredients.add(Ingredient(ingredientName, ingredientQuantity))
+                j++
+                ingredientName = cocktail.getString("strIngredient$j")
+            }
+            val image = cocktail.getString("strDrinkThumb")
+            cocktailsList.add(Cocktail(name, recipe, ingredients, image))
+        }
+        return cocktailsList
+    }
+
     constructor(parcel: Parcel) : this(
         parcel.readParcelable(SearchType::class.java.classLoader),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int
+        parcel.readString()
     )
 
 
@@ -55,7 +82,6 @@ class CocktailDBListBuilder(
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeParcelable(searchType, flags)
         parcel.writeString(searchString)
-        parcel.writeValue(resourceID)
     }
 
     override fun describeContents(): Int {
